6f4f96296f24bb6ec30d45c9471a32e07cb388fd
GerlakhA <andrey215gerlakh@gmail.com>
2025-04-12 16:48:19 +0300

    change next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next'
 import createNextIntlPlugin from 'next-intl/plugin'
 
 const nextConfig: NextConfig = {
-	output: 'export'
+	output: 'standalone'
 }
 
 const withNextIntl = createNextIntlPlugin('./src/config/i18n/request.ts')
