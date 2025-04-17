0f170f380bdb909a15b04fa7c7a56aec0fc9d7a0
GerlakhA <andrey215gerlakh@gmail.com>
2025-04-17 21:41:38 +0300

    change text size

--- src/shared/ui/about/index.tsx
+++ src/shared/ui/about/index.tsx
@@ -14,7 +14,7 @@ export const About = async () => {
 						initial={{ opacity: 0, y: -50 }}
 						animate={{ opacity: 1, y: 0 }}
 						transition={{ duration: 1, delay: 0.5 }}
-						className='text-7xl font-bold min-w-[400px]'
+						className='text-6xl font-bold min-w-[400px]'
 					>
 						{t('presentation')}
 					</motion.p>
